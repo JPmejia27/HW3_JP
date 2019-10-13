27cb6aa437dc08a72cc831d0e84aff5d81b02d57
Juan Mejia <mejiasil@usc.edu>
2019-10-13 15:23:25 -0700

    try quicksort

---
+++ problem2.cpp
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+#define        size       16*1024*1024
+
+void swap(int &a,int &b)
+{
+    std::cout << "A: " << a << " B: " << b << std::endl;
+    int x = a;
+    a = b;
+    b = x;
+    std::cout << "A: " << a << " B: " << b << std::endl;
+}
+int partition(int array[], int start, int end)
+{
+    //starting pivot is always at last index
+    int pivot = array[end];
+    //index for index of earliest num found that is > pivot that should be swapped with
+    int smaller = start - 1;
+    for(int j = 0; j < end; j++)
+    {
+        if(array[j] <= pivot)
+        {
+            smaller++;
+            swap(array[smaller],array[j);
+        }
+    }
+}
+void quickSort(int *array, int start, int end)
+{
+    // you quick sort function goes here
+    if(low < high)
+    {
+        int pIndex = partition(array,start,end);
+        
+        //bottom half
+        quickSort(array, start, pIndex-1);
+        //top half
+        quickSort(array,pIndex+1,end);
+    }
+    
+}
+
+int main(void){
+    int i, j, tmp;
+    struct timespec start, stop;
+    double exe_time;
+    srand(time(NULL));
+    int* m = new int[size];
+    for(i=0; i<size; i++){
+        m[i]=size-i;
+    }
+    
+    if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+    ////////**********Your code goes here***************//
+    
+    
+    
+    ///////******************************////
+    
+    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}
+    exe_time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+    
+    for(i=0;i<16;i++) printf("%d ", m[i]);
+    printf("\nExecution time = %f sec\n",  exe_time);
+}
