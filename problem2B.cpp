3ab3f648c6806a277b1dcf105034ed0704b5ee74
Juan Mejia <mejiasil@usc.edu>
2019-10-14 14:12:43 -0700

    zebra 2

--- problem2B.cpp
+++ problem2B.cpp
@@ -2,55 +2,47 @@
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
-#include <iostream>
 
-#define        size       16*1024*1024
+#define        size       16 * 1024 * 1024
 
-void swap(int &a,int &b)
+void swap(int *a,int *b)
 {
-    int x = a;
-    a = b;
-    b = x;
+    int x = *a;
+    *a = *b;
+    *b = x;
 }
-int partition(int array[], int start, int end)
+int partition(int *array, int start, int end)
 {
     //starting pivot is always at last index
-    int pivot = array[rand()%size];
+    int pivot = array[end];
     //index for index of earliest num found that is > pivot that should be swapped with
-    int smaller = start - 1;
+    int smaller = start;
     for(int j = start; j < end; j++)
     {
         if(array[j] < pivot)
         {
+            swap(&array[smaller],&array[j]);
             smaller++;
-            swap(array[smaller],array[j]);
         }
     }
-    swap(array[smaller+1],array[end]);
-    return(smaller + 1);
+    swap(&array[smaller],&array[end]);
+    return(smaller);
 }
-void quickSort(int *array, int lo, int hi)
+int quickSort(int *array, int lo, int hi)
 {
     // you quick sort function goes here
+    
+    //    printf("Low is %d and high is %d",lo,hi);
     if(lo < hi)
     {
-        int pIndex = partition(array,lo,hi);
+        pIndex = partition(array,lo,hi);
         
-        omp_set_num_threads(2);
-        #pragma omp parallel shared(array,lo,hi) private(pIndex)
-        {
-            ////////**********Use OpenMP to parallize this loop***************//
-            #pragma omp sections nowait
-            {
-                #pragma omp section
-                quickSort(array, lo, pIndex-1);
-                
-                #pragma omp section
-                quickSort(array,pIndex+1,hi);
-            }
-        }
+        //bottom half
+        quickSort(array, lo, pIndex-1);
+        //top half
+        quickSort(array,pIndex+1,hi);
     }
-    
+    return 0;
 }
 int main(void)
 {
@@ -59,15 +51,20 @@ int main(void)
     struct timespec start, stop;
     double exe_time;
     srand(time(NULL));
-    int* m = new int[size];
+    int * m = new int[size];
     for(i=0; i<size; i++){
         m[i]=size-i;
     }
     
+    
+    for(i=0;i<10;i++) printf("%d ", m[i]);
+    
     if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
     
     ////////**********Your code goes here***************//
+    
     quickSort(m,0,size-1);
+    
     
     
     ///////******************************////
@@ -76,7 +73,6 @@ int main(void)
     exe_time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
     
     for(i=0;i<16;i++) printf("%d ", m[i]);
+    //    for(i=0;i<10;i++) printf("%d ", m[i]);
     printf("\nExecution time = %f sec\n",  exe_time);
 }
-
-
